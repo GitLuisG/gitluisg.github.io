2eb3734a41f071e9338e4fe28386ebb5be1be5f3
Luis Gerardo Perales Torres <46802580+GitLuisG@users.noreply.github.com>
2025-05-29 20:56:29 -0600

    Actualizar FooterComponent.js

--- template/views/FooterComponent.js
+++ template/views/FooterComponent.js
@@ -17,11 +17,7 @@ class FooterComponent extends React.Component {
     return (
       <footer className="main-footer ">
         {" "}
-        <strong>
-          {" "}
-          Copyright &copy; 2014-2021{" "}
-          <a href="https://adminlte.io">AdminLTE.io</a>.{" "}
-        </strong>{" "}
+        {" "}
         All rights reserved.{" "}
         <div className="float-right d-none d-sm-block">
           {" "}
