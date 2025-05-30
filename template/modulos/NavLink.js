cf8621d31e7fea9824d66a44eed1368a0b2cd0ed
Luis Gerardo Perales Torres <46802580+GitLuisG@users.noreply.github.com>
2025-05-30 06:33:55 -0600

    Actualizar NavLink.js

--- template/modulos/NavLink.js
+++ template/modulos/NavLink.js
@@ -11,7 +11,6 @@ class NavLink extends React.Component {
           "col-md-3 col-sm-6 col-12"
         );
         break;
-
       case "Proyectos":
         return row(
           [
@@ -104,15 +103,12 @@ class NavLink extends React.Component {
           "col-sm-3"
         );
         break;
-
       case "Zona de Juegos":
         return '<div class="swiper mySwiper"> <div class="swiper-wrapper"> <div class="swiper-slide" style="width: 172.66699999999997px;"> <div class="position-relative p-3 bg-gray"> <img src="https://swiperjs.com/images/n4w-logo.svg" alt="Photo 1" class="img-fluid"> <div class="ribbon-wrapper ribbon-xl"> <div class="ribbon bg-success text-lg"> Brawl Start </div> </div> </div> <div class="position-relative p-3 bg-gray"> <img src="https://swiperjs.com/images/n4w-logo.svg" alt="Photo 1" class="img-fluid"> <div class="ribbon-wrapper ribbon-xl"> <div class="ribbon bg-success text-lg"> Brawl Start </div> </div> </div> <div class="position-relative p-3 bg-gray"> <img src="https://swiperjs.com/images/n4w-logo.svg" alt="Photo 1" class="img-fluid"> <div class="ribbon-wrapper ribbon-xl"> <div class="ribbon bg-success text-lg"> Brawl Start </div> </div> </div> <div class="position-relative p-3 bg-gray"> <img src="https://swiperjs.com/images/n4w-logo.svg" alt="Photo 1" class="img-fluid"> <div class="ribbon-wrapper ribbon-xl"> <div class="ribbon bg-success text-lg"> Brawl Start </div> </div> </div> <div class="position-relative p-3 bg-gray"> <img src="https://swiperjs.com/images/n4w-logo.svg" alt="Photo 1" class="img-fluid"> <div class="ribbon-wrapper ribbon-xl"> <div class="ribbon bg-success text-lg"> Brawl Start </div> </div> </div> </div> </div> <div class="swiper-pagination"></div> </div>';
         break;
-
         case "Articulos":
           return "Hola";
           break;
-
       case "Presentacion":
         return (
           ' <div class="row"> <div class="col-md-3"> <div class="card card-primary card-outline"> <div class="card-body box-profile"> <div class="text-center"> <img class="profile-user-img img-fluid img-circle" src="' + Assets + '/img/unnamed.jpg" /> </div> <h3 class="profile-username text-center">' +
@@ -123,7 +119,6 @@ class NavLink extends React.Component {
           buttonlink("https://github.com/GitLuisG", "Ir a Github") +
           ' </div> </div> <div class="card card-primary"> <div class="card-header"> <h3 class="card-title">Mas Informacion</h3> </div> <div class="card-body"> <strong><i class="fas fa-book mr-1"></i>Formacion Académica</strong> <p class="text-muted"> 2017 – 2021<br /> Licenciatura en Ingeniería en Tecnologías de la información<br /> Universidad politécnica de victoria<br /> </p> <hr /> <strong ><i class="fas fa-map-marker-alt mr-1"></i> Contacto y Ubicacion </strong> <p class="text-muted">Mayapan 818, Victoria, Tamaulipas</p> <a href="https://wa.me/+528342685756" class="text-muted"><ion-icon name="logo-whatsapp"></ion-icon>+52 (834) 268 575 6</a><br><a href="https://wa.me/+522351250081" class="text-muted"><ion-icon name="logo-whatsapp"></ion-icon>+52 (235) 125 00 81</a> <p class="text-muted">correousertb@gmail.com</p> <hr /> <strong><i class="far fa-file-alt mr-1"></i> Resumen</strong> <p class="text-muted"> Tengo amplia experiencia en análisis y limpieza de datos, wireframing y mantenimiento de sistemas informáticos, con un amplio dominio de sistemas operativos Windows y Linux. </p> </div> </div> </div> <div class="col-md-9"> <div class="card"> <div class="card-header p-2"> <ul class="nav nav-pills"> <li class="nav-item"> <a class="nav-link active" href="#Blog" data-toggle="tab" >Blog</a > </li> </ul> </div> <div class="card-body"> <div class="tab-content"><div class="active tab-pane" id="activity">' +
           post([
-            
             [
               "https://controlesdti.com/SCHFP/public/img/Logop_2.png",
               "ControleDti",
@@ -179,12 +174,10 @@ case "Sobre Mi":
           " </div></div> </div> </div> </div> </div> "
         );
         break;
-
       default:
         break;
     }
   }
-
   handleClick(i) {
     var navlink = document.getElementsByClassName("nav-link");
     for (let index = 0; index < navlink.length; index++) {
@@ -203,18 +196,15 @@ case "Sobre Mi":
         panel.innerHTML = " ";
         panel.innerHTML = this.obtenerPantalla(this.props.name);
         break;
-
       case "Proyectos":
         panel.innerHTML = " ";
         panel.innerHTML = this.obtenerPantalla(this.props.name);
         break;
-
       case "Sobre Mi":
         panel.innerHTML = " ";
         panel.innerHTML +=
           blockquote("Curriculum") + this.obtenerPantalla(this.props.name);
         break;
-
       case "Articulos":
         panel.innerHTML = " ";
         getDatos().forEach(element => {
@@ -224,10 +214,10 @@ case "Sobre Mi":
         break;
       case "Zona de Juegos":
         panel.innerHTML = " ";
-        panel.innerHTML += this.obtenerPantalla(this.props.name);
+      panel.innerHTML += this.obtenerPantalla(this.props.name);
         break;
        case "Presentacion":
-panel.innerHTML = " ";
+        panel.innerHTML = " ";
         this.obtenerPantalla(this.props.name);
         break;
       default:
@@ -235,17 +225,14 @@ panel.innerHTML = " ";
         break;
     }
   }
-
   icon() {
     switch (this.props.name) {
       case "Tablero":
         return <i className="nav-icon fas fa-tachometer-alt"></i>;
         break;
-
       case "Proyectos":
         return <i className="nav-icon far fa-plus-square"></i>;
         break;
-
       case "Sobre Mi":
         return <i className="nav-icon fas fa-book"></i>;
         break;
@@ -272,7 +259,6 @@ panel.innerHTML = " ";
           onClick={(i) => this.handleClick(i)}
         >
           {this.icon()}
-
           <p> {this.props.name}</p>
         </a>
       </li>
