66dd9f1dc7c224cffdfd8fae87d832508018479d
GitLuisG <correousertb@gmail.com>
2025-06-08 03:55:29 -0600

    Última Sincronización: 2025-06-08 09:55:29 (Móvil)

--- Modules/Home/app/core/settings.js
+++ Modules/Home/app/core/settings.js
@@ -1,10 +1,12 @@
 
 // Definir el uso horario o timezone del sistema
+const Module_Name = 'Home';
+
 const FUNCTIONS = 'app/functions/';
 
 const TOKEN = '';
 
-const URL = window.location.origin + "/Modules/";
+const URL = window.location.origin + "/Modules/" + Module_Name + "/";
 const PATH = window.location.pathname;
 const Dir = URL + PATH;
 const Assets = URL + "assets/";
