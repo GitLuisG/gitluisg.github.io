fce3393ec8f2254bd44ad2f0982a589ad907dc4f
GitLuisG <correousertb@gmail.com>
2025-06-08 17:11:19 -0600

    Última Sincronización: 2025-06-08 23:11:19 (Móvil)

--- Modules/TrasmisiónTV/template/views/SectionComponent.js
+++ Modules/TrasmisiónTV/template/views/SectionComponent.js
@@ -2,7 +2,7 @@ class SectionComponent extends React.Component {
     render() {
       return <section id="mainSection">
         <Logo/>
-        <video class="img-fluid" src="https://videos.pexels.com/video-files/3195394/3195394-uhd_2560_1440_25fps.mp4">
+        <video class="img-fluid" controls src="https://videos.pexels.com/video-files/3195394/3195394-uhd_2560_1440_25fps.mp4">
           
         </video>
       </section>;
